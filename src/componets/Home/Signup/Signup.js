37773c7799a86e7f65efa2507fffd5f346d67f4f
MKASIF <mdasifkhan089@gmail.com>
2022-04-18 01:40:12 +0600

    Create user with email and password done

--- src/componets/Home/Signup/Signup.js
+++ src/componets/Home/Signup/Signup.js
@@ -1,8 +1,25 @@
+import { useRef } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase/firebase.init';
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 
 const Signup = () => {
-
+  const naviget = useNavigate()
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const handleSignUpForm = (event) => {
+    event.preventDefault();
+    let email = emailRef.current.value;
+    let password = passwordRef.current.value;
+    createUserWithEmailAndPassword(email, password)
+  };
+  if (user) {
+    naviget('/')
+  }
     return (
       <Container>
         <Row>
@@ -11,30 +28,40 @@ const Signup = () => {
             <Form>
               <Form.Group className="mb-3" controlId="formGroupName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control  type="text" placeholder="Enter Name" />
+                <Form.Control
+                  ref={nameRef}
+                  type="text"
+                  placeholder="Enter Name"
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formGroupEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control  type="email" placeholder="Enter email" required />
+                <Form.Control
+                  ref={emailRef}
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
+                  ref={passwordRef}
                   type="password"
                   placeholder="Enter Password"
                   required
                 />
               </Form.Group>
               <button
+                onClick={handleSignUpForm}
                 className="all-service-btn mt-2 text-uppercase"
                 type="submit"
-                
               >
                 Sign Up
               </button>
             </Form>
             <p className="mt-2">
-              If you already have an account,{" "}
+              If you already have an account,
               <Link to={"/login"}>Just LogIn</Link>
             </p>
           </Col>
